d5e302bb59cac90c1ba77faf0ae3c2c36ba6bba4
Florian Liehr <liehr.florian@fh-swf.de>
2024-08-13 15:55:24 +0200

    fix: input validation of spotify url

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ from src.Controllers.BeatSaverController import BeatSaverController
 from src.Services.SpotifyService import SpotifyService
 from src.Services.BeatSaverService import BeatSaverService
 
-
 if __name__ == "__main__":
     playlist_url = input("Enter Spotify playlist URL: ")
+
+    # Check if the playlist URL is valid spotify playlist url
+    # Scheme: https://open.spotify.com/playlist/{playlist_id}
+
+    if not playlist_url.startswith("https://open.spotify.com/playlist/"):
+        print("Invalid Spotify playlist URL")
+        exit(1)
 
     spotify_service = SpotifyService()
     spotify_controller = SpotifyController(spotify_service)
